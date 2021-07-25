26521c3fda2bbce965c25b15c7a2a08213eaf92d
Gabriel Fleming <gabrielflemingac@gmail.com>
2021-07-25 17:49:04 -0300

    integrando com BD

--- src/main/java/br/com/api/streamvideo/entity/Video.java
+++ src/main/java/br/com/api/streamvideo/entity/Video.java
@@ -1,7 +1,14 @@
 package br.com.api.streamvideo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="video")
 public class Video {
 
+    @Id
     Long id;
     String titulo;
     String descricao;
